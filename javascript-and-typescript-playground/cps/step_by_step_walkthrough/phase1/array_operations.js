261e63c323a7176899a39fabd59b73bdc90a9865
Krishna <krishnamurugan589@gmail.com>
2025-05-24 08:35:57 +0530

    phase 1 CPS exercise of array function implementations

--- javascript-and-typescript-playground/cps/step_by_step_walkthrough/phase1/array_operations.js
+++ javascript-and-typescript-playground/cps/step_by_step_walkthrough/phase1/array_operations.js
@@ -76,3 +76,58 @@ reduceCPS(
     1,
     console.log
 );
+
+
+// Direct style implementation filter (recursive)
+function filter(f, lst) {
+    if (typeof f !== 'function') throw new TypeError("Expected an argument of type function");
+    if (!Array.isArray(lst)) throw new TypeError("Expected an argument of type Array");
+    // Base case
+    if (lst.length === 0) return [];
+    else {
+        // Recursive case
+        const [head, ...tail] = lst;
+        if (f(head)) {
+            return [head, ...filter(f, tail)];
+        } else {
+            return filter(f, tail);
+        }
+    }
+}
+
+console.log(filter(x => x % 2 !== 0, []))
+console.log(filter(x => x % 2 === 0, [10, 21, 30, 41, 50, 61, 70, 81, 90, 101]))
+
+
+// CPS style implementation of filter (recursive)
+function filterCPS(f, lst, k) {
+    if (typeof f !== 'function' || typeof k !== 'function') throw new TypeError("Expected an argument of type function");
+    if (!Array.isArray(lst)) throw new TypeError("Expected an argument of type Array");
+    // Base case
+    if (lst.length === 0) k([]);
+    else {
+        // Recursive case
+        const [head, ...tail] = lst;
+        filterCPS(f, tail, function (r) {
+            if (f(head)) {
+                console.log(`r inside if: ${r}`)
+                k([head, ...r])
+            } else {
+                console.log(`r inside else: ${r}`)
+                k(r);
+            }
+        })
+    }
+}
+
+filterCPS(
+    x => x % 2 === 0,
+    [],
+    console.log
+);
+
+filterCPS(
+    x => x % 2 === 0,
+    [1,2,3,4,5,6,7,8,9],
+    console.log
+);
